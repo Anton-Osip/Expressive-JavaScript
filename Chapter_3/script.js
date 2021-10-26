0a65ef88dfc62ef148c5912b225e3095d154ca3f
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2021-10-26 21:50:23 +0300

    Chapter_3

---
+++ Chapter_3/script.js
@@ -0,0 +1,46 @@
+console.log('Минимум')
+console.log('-----------------------------------------')
+function min(a, b) {
+	if (b === undefined) {
+		return a
+	} else if (a < b) {
+		return a
+	} else {
+		return b
+	}
+}
+console.log(min(5, 6))
+console.log(min(8, 6))
+console.log(min(8))
+console.log('-----------------------------------------')
+console.log('-----------------------------------------')
+console.log('Рекурсия')
+console.log('-----------------------------------------')
+
+function isEven(num) {
+	if (num === 0) {
+		return true
+	} else if (num < 0) {
+		return false
+	} else {
+		return isEven(num - 2)
+	}
+}
+console.log(isEven(60))
+console.log(isEven(75))
+console.log('-----------------------------------------')
+console.log('-----------------------------------------')
+console.log('Подсчет букв')
+console.log('-----------------------------------------')
+
+let str = 'BfghjBfghjkBukjB'
+function countBs(str, leter) {
+	let count = 0
+	for (let i = 0; i < str.length; i++) {
+		str[i] === leter ? count++ : count
+	}
+	return count
+}
+
+console.log(countBs(str, 'f'))
+console.log('-----------------------------------------')
